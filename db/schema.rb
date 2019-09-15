e65d49294c26a1219ac514b6f4894450e629fd2b
Yosuke Tsuchiya <48376551+tchmr@users.noreply.github.com>
2019-09-15 17:12:15 +0900

    Revert "Rails 6.0.0 へアップデート"

--- db/schema.rb
+++ db/schema.rb
@@ -27,7 +27,6 @@ ActiveRecord::Schema.define(version: 2019_05_21_113538) do
   create_table "groups", force: :cascade do |t|
     t.string "name", null: false
     t.integer "leader", null: false
-    t.date "quest_start_ymd"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -80,6 +79,7 @@ ActiveRecord::Schema.define(version: 2019_05_21_113538) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "users_name_key", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
